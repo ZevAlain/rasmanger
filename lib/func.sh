249a309201e97a615bce5fbda5346dcf478c7cac
Alain <zevalain@hotmail.com>
2019-01-26 00:43:34 +0800

    增加功能

--- lib/func.sh
+++ lib/func.sh
@@ -4,15 +4,33 @@ showCommandUsager()
     echo "Use : rasmanger -s cpu"
 }
 
-showcputmp()
-{
-    if [ "${1}" != "cpu" ];then
-        showCommandUsager
-        exit 1
-    fi
-    NowCpuTem=`cat /sys/class/thermal/thermal_zone0/temp`
-    CurrentNowCpuTem=`expr ${NowCpuTem} / 1000`
 
-    echo "Now CPU Tem :" ${CurrentNowCpuTem} "%"
+showcputemp()
+{
+    if [  -n ${1}  ];then
+        showcputemp
+        showgputemp
+        return 
+    else if [ ${1} == "cpu" ]
+        showcputemp
+        return
+    else if ["${1}" == "gpu" ]
+        showgputemp
+    else
+        showCommandUsager
+    fi
+
 }
 
+showcputemp()
+{
+        NowCpuTem=`cat /sys/class/thermal/thermal_zone0/temp`
+        CurrentNowTem=`expr ${NowCpuTem} / 1000`
+        echo "Now CPU Temp :" ${CurrentNowCpuTem} "'C"
+}
+
+showgputemp()
+{
+        CurrentNowTem=`/opt/vc/bin/vcgencmd measure_temp | cut -f2 -d"="`
+        echo "Now GPU Temp :" ${CurrentNowCpuTem}
+}
